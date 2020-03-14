30a00a423e9bdde20c675a7f131da3f8840bcd6f
Apeksha Rajkumari <apeksharajkumari@gmail.com>
2020-03-13 22:31:52 -0700

    Adding code to check one-to-one mapping in strings

---
+++ main.py
@@ -0,0 +1,33 @@
+
+s1 = input()
+s2 = input()
+ans = True
+if len(s2) < len(s1):
+    ans = False
+else:
+    one = {}
+    two = {}
+    for c in s1:
+        if c in one:
+            one[c] = one[c]+1
+        else:
+            one[c] = 1
+
+    for c in s2:
+        if c in two:
+            two[c] = two[c]+1
+        else:
+            two[c] = 1
+
+    for k1,v1 in one.items():
+        for k2,v2 in two.items():
+            if v1 == v2 :
+                one[k1] = 0
+                two[k2] = 0
+                break
+
+    for val in one.values():
+        if val > 1 :
+            ans = False
+
+print(ans)
