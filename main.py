016df958c74d922a18acfed184b9a5568342cdd8
Apeksha Rajkumari <apeksharajkumari@gmail.com>
2020-03-13 22:50:12 -0700

    Added ability to parse command line arguments

--- main.py
+++ main.py
@@ -1,33 +1,36 @@
-
-s1 = input()
-s2 = input()
-ans = True
-if len(s2) < len(s1):
-    ans = False
+import sys
+if(len(sys.argv)<3):
+    print('Error')
 else:
-    one = {}
-    two = {}
-    for c in s1:
-        if c in one:
-            one[c] = one[c]+1
-        else:
-            one[c] = 1
+    s1 = sys.argv[1]
+    s2 = sys.argv[2]
+    ans = True
+    if len(s2) < len(s1):
+        ans = False
+    else:
+        one = {}
+        two = {}
+        for c in s1:
+            if c in one:
+                one[c] = one[c]+1
+            else:
+                one[c] = 1
 
-    for c in s2:
-        if c in two:
-            two[c] = two[c]+1
-        else:
-            two[c] = 1
+        for c in s2:
+            if c in two:
+                two[c] = two[c]+1
+            else:
+                two[c] = 1
 
-    for k1,v1 in one.items():
-        for k2,v2 in two.items():
-            if v1 == v2 :
-                one[k1] = 0
-                two[k2] = 0
-                break
+        for k1,v1 in one.items():
+            for k2,v2 in two.items():
+                if v1 == v2 :
+                    one[k1] = 0
+                    two[k2] = 0
+                    break
 
-    for val in one.values():
-        if val > 1 :
-            ans = False
+        for val in one.values():
+            if val > 1 :
+                ans = False
 
-print(ans)
+    print(ans)
